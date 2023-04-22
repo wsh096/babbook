43cf33a8eaca2b0a0b60ab0765236f56110f229d
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-23 02:08:25 +0900

    레스토랑의 비즈니스넘버 부분 수정 브랜치를 다시 돌아가면 해당 값의 전으로 돌아간다는 걸 확인함.

--- src/main/java/com/zerobase/babbook/domain/entity/Restaurant.java
+++ src/main/java/com/zerobase/babbook/domain/entity/Restaurant.java
@@ -1,17 +1,17 @@
 package com.zerobase.babbook.domain.entity;
 
 import com.zerobase.babbook.domain.form.RestaurantForm;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -37,12 +37,13 @@ public class Restaurant extends BaseEntity{
     private String name;
     @Column(length = 500)
     private String description;
-
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{5}", message = "사업자 등록 번호 형식이 유효하지 않습니다.")
+    @ApiModelProperty(value = "사업자 등록 번호 (XXX-XX-XXXXX)", example = "123-45-67890", required = true)
     private String businessNumber;
     private String address;
     private String phone;
 
-    public static Restaurant of(RestaurantForm form,Owner owner) {
+    public static Restaurant of(RestaurantForm form, Owner owner) {
         return Restaurant.builder()
             .name(form.getName())
             .businessNumber(form.getBusinessNumber())
@@ -51,5 +52,10 @@ public class Restaurant extends BaseEntity{
             .phone(form.getPhone())
             .owner(owner)
             .build();
+    }
+
+    public boolean isValidBusinessNumber() {
+        // XXX-XX-XXXXX 형식의 유효성 검사
+        return businessNumber != null && businessNumber.matches("\\d{3}-\\d{2}-\\d{5}");
     }
 }
